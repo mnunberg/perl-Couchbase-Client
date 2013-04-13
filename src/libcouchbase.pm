89415041c4b57470da0c5ed464c4938fcc6bf897
M. Nunberg <mnunberg@haskalah.org>
2013-04-13 15:05:14 +0100

    Add stub file for 'Couchbase::libcouchbase'

---
+++ src/libcouchbase.pm
@@ -0,0 +1,12 @@
+package Couchbase::libcouchbase;
+use strict;
+use warnings;
+use File::ShareDir;
+
+our $VERSION = '2.0.5';
+
+__END__
+
+=head1 NAME
+
+Couchbase::libcouchbase - heavy lifting for libcouchbase shared object dependencies
