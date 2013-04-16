0eadd7c0746b8065e52b066971662fac8603b1fa
Mark Nunberg <mnunberg@haskalah.org>
2013-04-16 14:03:00 -0700

    Download mock if not available

--- PLCB_Config.pm
+++ PLCB_Config.pm
@@ -11,8 +11,9 @@ use Hash::Util qw(lock_keys);
 
 my $params = {
     #URL from which to download the mock JAR file for tests
-    COUCHBASE_MOCK_JARURL => 'https://github.com/downloads/mnunberg/' .
-        'perl-Couchbase-Client/CouchbaseMock-0.5-SNAPSHOT.jar',
+    COUCHBASE_MOCK_JARURL => 'http://files.couchbase.com/'.
+                            'maven2/org/couchbase/mock/CouchbaseMock/0.5-SNAPSHOT/'.
+                            'CouchbaseMock-0.5-20120726.220757-19.jar',
 
     #version numbers for libcouchbase and libvbucket
     LIBCOUCHBASE_RELEASE => '2.0.5_2_gacffe1a',
--- t/01-main.t
+++ t/01-main.t
@@ -9,8 +9,15 @@ my $config = do 'PLCB_Config.pm';
 use Couchbase::Test::Common;
 my $TEST_PORT;
 
+my $jarurl = $config->{COUCHBASE_MOCK_JARURL};
+my $jarfile = __DIR__ . "/tmp/CouchbaseMock.jar";
+if (! -e $jarfile) {
+    warn("Can't find JAR. Downloading.. $jarurl");
+    system("wget -O $jarfile $jarurl");
+}
+
 Couchbase::Test::Common->Initialize(
-    jarfile => __DIR__ . "/tmp/CouchbaseMock.jar",
+    jarfile => $jarfile,
     nodes => 5,
     buckets => [{name => "default", type => "memcache"}],
 );
