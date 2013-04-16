35b2e2ba2f1f76533966d77eb7d0ff8b30087659
Mark Nunberg <mnunberg@haskalah.org>
2013-04-16 16:38:40 -0700

    Replace do_make with enhanced perl version

---
+++ author_utils/do_make.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+GetOptions(
+    'p|prefix=s' => \my $Prefix,
+    'cc=s' => \my $CC,
+    'j=i' => \my $JOBS,
+    'w|warnings' => \my $WALL
+);
+
+$Prefix ||= "/sources/libcouchbase/inst";
+
+system("make distclean");
+
+my $cmd = "perl Makefile.PL ";
+$cmd .= sprintf("--incpath='-I%s/include' ", $Prefix);
+$cmd .= sprintf("--libpath='-L%s/lib' ", $Prefix);
+if ($CC) {
+    $cmd .= "CC=$CC ";
+}
+
+if ($WALL) {
+    $cmd .= "OPTIMIZE=-Wall";
+}
+
+system($cmd);
+
+my $mcmd = "make ";
+if ($JOBS) {
+    $mcmd .= "-j$JOBS";
+}
+
+system($mcmd);
