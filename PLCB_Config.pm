7aa42f21f42f60ba3d7526a977d48f8345e72c14
Mark Nunberg <mnunberg@haskalah.org>
2014-07-08 07:42:05 -0700

    Update to latest mock

--- PLCB_Config.pm
+++ PLCB_Config.pm
@@ -11,9 +11,7 @@ use Hash::Util qw(lock_keys);
 
 my $params = {
     #URL from which to download the mock JAR file for tests
-    COUCHBASE_MOCK_JARURL => 'http://files.couchbase.com/'.
-                            'maven2/org/couchbase/mock/CouchbaseMock/0.5-SNAPSHOT/'.
-                            'CouchbaseMock-0.5-20120726.220757-19.jar',
+    COUCHBASE_MOCK_JARURL => 'http://files.couchbase.com/maven2/org/couchbase/mock/CouchbaseMock/0.8-SNAPSHOT/CouchbaseMock-0.8-20140621.030439-1.jar',
 
     #version numbers for libcouchbase and libvbucket
     LIBCOUCHBASE_RELEASE => '2.3.2',
