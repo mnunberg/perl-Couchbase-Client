ece311032a61f4f1a0ef32776dd6d4ccbccbff52
Mark Nunberg <mnunberg@haskalah.org>
2014-09-26 18:03:07 -0700

    Update MANIFEST

--- author_utils/gen_manifest.pl
+++ author_utils/gen_manifest.pl
@@ -7,7 +7,6 @@ use lib __DIR__;
 my $config = do 'PLCB_Config.pm' or die $@;
 
 my $couchbase_release = $config->{LIBCOUCHBASE_RELEASE};
-my $libevent_release = $config->{LIBEVENT_RELEASE};
 
 open my $infh, "<", "MANIFEST.in";
 open my $outfh, ">", "MANIFEST";
@@ -15,7 +14,6 @@ open my $outfh, ">", "MANIFEST";
 foreach my $line (<$infh>) {
     next unless $line;
     $line =~ s/__LIBCOUCHBASE_RELEASE__/$couchbase_release/g;
-    $line =~ s/__LIBEVENT_RELEASE__/$libevent_release/g;
     print $outfh $line;
 }
 
