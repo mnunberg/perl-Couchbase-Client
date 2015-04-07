5ca3c376fc8ab9c9069461380859511269969470
Mark Nunberg <mnunberg@haskalah.org>
2015-04-07 09:51:52 -0700

    Update landing page documentation

--- lib/Couchbase.pm
+++ lib/Couchbase.pm
@@ -81,49 +81,67 @@ __END__
 
 =head1 NAME
 
-Couchbase - Perl client for L<Couchbase|http://www.couchbase.com>
-
-This is the Couchbase client for Perl. It replaces the older L<Couchbase::Client>.
-
-It depends on L<libcouchbases|http://couchbase.com/communities/c>.
-
-
-=head2 SYNOPSIS
-
-
-    use Couchbase;
-    my $cluster = Couchbase::Cluster->new("couchbase://host1,host2,host3");
-    my $bkt_defl = $cluster->open_bucket("default");
-    my $bkt_beer = $cluster->open_bucket("beer-sample");
-    my $bkt_other = $cluster->open("other_bucket?config_cache=/foo/bar/baz.cache");
+Couchbase - Couchbase Client Library
 
 
 =head2 DESCRIPTION
 
+B<You probably want to see L<Couchbase::Bucket> for the actual documentation>.
 
-The C<Couchbase::Cluster> class represents a top level object which can be used
-to store common connection settings about an L<Couchbase::Bucket>.
-
-
-=head3 CONSTRUCTOR
-
-The constructor accepts a URI-like format (see the constructor for
-L<Couchbase::Bucket>), but I<without> the bucket name itself being specified.
+This document contains general information about the module and installation
+instructions
 
 
-=head2 open_bucket
+=head3 INSTALLING
 
-This returns a connected bucket based on the bucket name. The bucket name
-itself may be followed by a C<?> and several I<options>, (like the
-connection string itself).
+This module depends on the Couchbase C library
+(L<libcouchbase|http://docs.couchbase.com/developer/c-2.4/download-install.html>);
+please ensure this module is installed before attempting to install this
+module.
 
-
-If an open bucket already exists with the given connection string, that bucket
-is returned rather than opening a new one.
-
-
-
-=head1 SEE ALSO
+If you can't use C extensions, Couchbase has a pure memcached emulation
+layer called C<moxi> - which can then be used with any traditional Memcached
+client (and in fact, this is what I used before writing this module).
 
 
-L<Couchbase::Bucket> - Main bucket class.
+=head3 EXAMPLES
+
+Examples may be found in the C<eg> directory of the module. Other examples
+can be found in the tests, which are actually in C<lib/Couchbase/Test>.
+
+
+=head3 RELATION TO L<Couchbase::Client>
+
+This module B<replaces> the older L<Couchbase::Client> module. Development
+on the latter has ceased, ans it is no longer maintained. It is maintained
+on CPAN purely so existing code using the module continues to function.
+
+This module represents a complete rewrite of the older module
+
+
+=head3 SUPPORT
+
+While sponsored by Couchbase, this module is not officially supported by
+Couchbase, Inc. Issues and problems may be reported in the following
+venues.
+
+
+=over
+
+=item *
+
+L<Github project page|https://github.com/mnunberg/perl-Couchbase-Client>
+
+=item *
+
+Couchbase mailing list (C<couchbase at googlegroups dot com>)
+
+=item *
+
+C<#libcouchbase> IRC channel on freenode
+
+=item *
+
+Perl's RT bug tracker (see the link on the cpan page).
+
+=back
